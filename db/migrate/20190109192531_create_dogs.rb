0a6aa053e18d81bbdd65b71aac360c78290d4203
Sharmaine Requerme <srqrme@gmail.com>
2019-01-09 19:29:23 +0000

    Done.

--- db/migrate/20190109192531_create_dogs.rb
+++ db/migrate/20190109192531_create_dogs.rb
@@ -1,16 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
 
-  #create table with name and breed columns
-  def up
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
-  end
-
-  #drop table
-  def down
-    drop_table :dogs
   end
 
 end
