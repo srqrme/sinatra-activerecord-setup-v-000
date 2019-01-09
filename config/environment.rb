80016c9f99ffd4776f667c2e9796c786f3821300
Sharmaine Requerme <srqrme@gmail.com>
2019-01-09 19:24:40 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+#set up connection to sqlite3 database, "database.db"
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
